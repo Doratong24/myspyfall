6fd12f5e117b1812a78277a916b854db117e02c4
Doratong24 <doratong24@hotmail.com>
2018-10-05 12:28:06 +0700

    Add vote button

--- src/client.js
+++ src/client.js
@@ -77,7 +77,7 @@ microgear.on('message', function (topic, data) {
             if (i % 5 == 0) {
                 htmlText += '<div class="cols">';
             }
-            htmlText += '<div class="rows">' + place_list[i] + '</div>';
+            htmlText += '<div class="rows">' + place_list[i].name + '</div>';
 
             if ((i + 1) % 5 == 0) {
                 htmlText += '</div>';
